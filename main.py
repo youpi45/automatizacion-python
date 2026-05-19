610bee1f0cc7b3201d16e8575e2a51d32fe1d5f0
youpi45 <ayoub.bouzekkoura@outlook.es>
2026-05-19 09:34:01 +0200

    añadir programa de guardado de usuarios

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-print ("hola desde github y python")
+archivo = open("usuarios.txt", "a")
 
+while True:
+    nombre = input("Introduce un nombre (o 'salir'): ")
+
+    if nombre.lower() == "salir":
+        break
+
+    archivo.write(nombre + "\n")
+
+archivo.close()
+
+print("✅ Datos guardados correctamente")
